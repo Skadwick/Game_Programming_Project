81d13a976b4f3156f704a1f72a1c17c634f6716f
Joshua Shadwick <Skadwick@gmail.com>
2014-04-01 12:42:39 -0400

    Formatting, commenting, and minor changes

--- Game_Programming_Project/Game_Programming_Project/Level.cs
+++ Game_Programming_Project/Game_Programming_Project/Level.cs
@@ -9,24 +9,32 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Game_Programming_Project
 {
+
+
+    /// <summary>
+    /// This class is loaded by the Game class, and contains the player object, the block grid system and all
+    /// of the block objects, and enemy objects.  The content manager from the game class is passed here, so that
+    /// content can be loaded.
+    /// </summary>
+
     class Level
     {
-
         //Terrain structure of the level
         private Block[,] blocks; //2D array
-        int BlockGridHeight = 18;
-        int BlockGridWidth = 32;
+        const int BlockGridHeight = 18;
+        const int BlockGridWidth = 32;
 
-        //Player entities in the level.
+        //Player object which the user controls within the level
         public Player Player
         {
             get { return player; }
         }
         Player player;
 
-        private Vector2 start;
+        //Starting position of the player
+        private Vector2 start; 
 
-        // Level content       
+        //Game content       
         public ContentManager Content
         {
             get { return content; }
@@ -34,6 +42,13 @@ namespace Game_Programming_Project
         ContentManager content;
 
 
+        /// <summary>
+        /// Constructor for the Level class.  serviceProvider is the Game.Services property from the
+        /// Game class, which gets the the GameServiceContainer object holding all the service providers
+        /// attached to the Game. The Level class uses this to create a copy of the Game's content manager.
+        /// The constructor also begins loading the blocks needed for the level, and spawns the player at
+        /// the proper location within the level.
+        /// </summary>
         public Level(IServiceProvider serviceProvider)
         {
             content = new ContentManager(serviceProvider, "Content");
@@ -42,11 +57,14 @@ namespace Game_Programming_Project
         }
 
 
-        //Begins loading the layout of the various blocks of the level
+
+        /// <summary>
+        /// Begins creating Block objects based on a 2D char array which is hard-coded below (Should be read in from file).
+        /// The symbols in the char array determine the type of block, and it's collision type.
+        /// </summary>
         private void LoadBlocks()
         {
-            //Temporary map of chars which determine which block goes where.
-            //Eventually, this needs to be read in from a file.
+            //Creating a 2D array of characters which determine which block goes where
             char[,] blockMap = { 
                                {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
                                {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
@@ -63,12 +81,12 @@ namespace Game_Programming_Project
                                {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
                                {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
                                {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
-                               {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
+                               {'-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', 'x','x', 'x', 'x', 'x','x', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-','-', '-', '-', '-'},
                                {'x', 'x', 'x', 'x','x', 'x', 'x', 'x','x', 'x', 'x', 'x','-', '-', '-', '-','x', 'x', 'x', 'x','x', 'x', 'x', 'x','x', 'x', 'x', 'x','x', 'x', 'x', 'x'},
                                {'x', 'x', 'x', 'x','x', 'x', 'x', 'x','x', 'x', 'x', 'x','-', '-', '-', '-','x', 'x', 'x', 'x','x', 'x', 'x', 'x','x', 'x', 'x', 'x','x', 'x', 'x', 'x'}
                                };
 
-
+            //Initialize the block grid
             blocks = new Block[BlockGridWidth, BlockGridHeight];
 
             //Creating each of the blocks in the grid system
@@ -81,14 +99,17 @@ namespace Game_Programming_Project
                     blocks[x, y] = LoadBlock(blockType, x, y);
                 }
             }
-
         }
 
 
-        //Determines which type of block is needed, and where.  Then, creates the block.
+
+        /// <summary>
+        /// Recieves a char which it uses in a switch statement to decide which type of
+        /// block to create.  Once that determination is made, it creates the block and
+        /// returns it.
+        /// </summary>
         private Block LoadBlock(char blockType, int x, int y)
         {
-
             switch (blockType)
             {
                 //Air block
@@ -99,22 +120,28 @@ namespace Game_Programming_Project
                 case 'x':
                     return CreateBlock("block1", BlockCollision.Impassable);
 
+                //Default should only be reached if an incorrect char symbol was entered
                 default:
                     throw new Exception("Error loading block");
-
             }
-
-
         }
+        
 
 
-        //Loads an individual block and returns it
+        /// <summary>
+        /// Creates a new block with the proper collision type, and loads the texture based 
+        /// on the string argument.
+        /// </summary>
         private Block CreateBlock(string name, BlockCollision collision)
         {
             return new Block(Content.Load<Texture2D>("Blocks/" + name), collision);
         }
 
 
+
+        /// <summary>
+        /// dfhgdf
+        /// </summary>
         public void Update(GameTime gameTime, KeyboardState keyboardState)
         {
 
@@ -123,6 +150,10 @@ namespace Game_Programming_Project
         }
 
 
+
+        /// <summary>
+        /// asdfa
+        /// </summary>
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             //Draw player
@@ -134,9 +165,13 @@ namespace Game_Programming_Project
         }
 
 
+
+        /// <summary>
+        /// adsfa
+        /// </summary>
         public void DrawBlocks(SpriteBatch spriteBatch)
         {
-
+            //Looping through each of the blocks in the grid
             for (int y = 0; y < BlockGridHeight; y++)
             {
                 for (int x = 0; x < BlockGridWidth; x++)
@@ -144,14 +179,12 @@ namespace Game_Programming_Project
                     Texture2D blockTexture = blocks[x, y].Texture;
                     if (blockTexture != null)
                     {
-                        // Draw it in screen space.
+                        //Find the proper location of the block and draw it
                         Vector2 position = new Vector2(x, y) * Block.Size;
                         spriteBatch.Draw(blockTexture, position, Color.White);
                     }
-
                 }
             }
-
         }
 
 
